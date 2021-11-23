194563d2fe7bdecdbea84c611152b001568a60b5
Daan <daan@ukr.net>
2021-11-23 18:01:37 +0200

    small refactor

--- src/EasySlug.php
+++ src/EasySlug.php
@@ -2,77 +2,150 @@
 
 namespace EasySlug;
 
+use Exception;
 use Illuminate\Support\Str;
 
-trait EasySlug {
+/**
+ * @method static getKeyName()
+ * @method static where($column, string $value)
+ * @method getKey()
+ */
+trait EasySlug
+{
 
-  public function EasySlugCheck($value, $slug_field, $source = null) {
 
-    $primaryKey = static::getKeyName();
+    /**
+     * @param $value
+     * @param $slug_field
+     * @param null $source
+     * @return string
+     */
+    public function EasySlugCheck($value, $slug_field, $source = null)
+    {
+        $primaryKey = static::getKeyName();
 
-    if (empty($value)) {
-      if (isset($source)) {
-        if(isset($this->{$source}) && $this->{$source}) {
-          $value = Str::slug($this->{$source});
+        if (empty($value)) {
+
+            if (isset($source)) {
+
+                if (isset($this->{$source}) && $this->{$source}) {
+
+                    $value = Str::slug($this->{$source});
+
+                } else {
+
+                    $value = 'slug_' . date("Y-m-d_H-i-s");
+
+                }
+
+            } elseif ($this->title) {
+
+                $value = Str::slug($this->title);
+
+            } elseif ($this->name) {
+
+                $value = Str::slug($this->name);
+
+            } else {
+
+                $value = 'slug_' . date("Y-m-d_H-i-s");
+
+            }
+
         } else {
-          $value = 'slug_'. date("Y-m-d_H-i-s");
+
+            $value = Str::slug($value);
+
         }
-      } elseif ($this->title) {
-        $value = Str::slug($this->title);
-      } elseif ($this->name) {
-        $value = Str::slug($this->name);
-      } else {
-        $value = 'slug_'. date("Y-m-d_H-i-s");
-      }
-    } else {
-      $value = Str::slug($value);
+
+
+        $value = mb_strimwidth($value, 0, 100);
+
+
+        /**
+         * Check for unique
+         */
+        if ($this->getKey()) {
+
+            /** Existing value */
+            try {
+
+                $existing = static::where($slug_field, $value)->where($primaryKey, '!=',
+                    $this->getKey())->withTrashed()->count();
+
+            } catch (Exception $e) {
+
+                $existing = static::where($slug_field, $value)->where($primaryKey, '!=', $this->getKey())->count();
+
+            }
+
+
+            if ($existing) {
+
+                $x = 1;
+
+                try {
+
+                    $existing_while = static::where($slug_field, $value . '-' . $x)->where($primaryKey, '!=',
+                        $this->getKey())->withTrashed()->count();
+
+                } catch (Exception $e) {
+
+                    $existing_while = static::where($slug_field, $value . '-' . $x)->where($primaryKey, '!=',
+                        $this->getKey())->count();
+
+                }
+
+
+                while ($existing_while) {
+                    $x++;
+                }
+
+
+                $value .= '-' . $x;
+            }
+
+        } else {
+
+            /** New value */
+
+            try {
+
+                $new = static::where($slug_field, $value)->withTrashed()->count();
+
+            } catch (Exception $e) {
+
+                $new = static::where($slug_field, $value)->count();
+
+            }
+
+            if ($new) {
+
+                $x = 1;
+
+                try {
+
+                    $new_while = static::where($slug_field, $value . '-' . $x)->withTrashed()->count();
+
+                } catch (Exception $e) {
+
+                    $new_while = static::where($slug_field, $value . '-' . $x)->count();
+
+                }
+
+                while ($new_while) {
+
+                    $x++;
+
+                }
+
+
+                $value .= '-' . $x;
+            }
+        }
+
+
+        return $this->attributes[$slug_field] = $value;
     }
-
-    $value = mb_strimwidth($value, 0, 100);
-
-    //check for unique
-    if($this->getKey()) {
-      //existing
-      try {
-        $existing = static::where($slug_field, $value)->where($primaryKey, '!=', $this->getKey())->withTrashed()->count();
-      } catch (\Exception $e) {
-        $existing = static::where($slug_field, $value)->where($primaryKey, '!=', $this->getKey())->count();
-      }
-
-      if ($existing) {
-        $x = 1;
-        try {
-          $existing_while = static::where($slug_field, $value . '-' . $x)->where($primaryKey, '!=', $this->getKey())->withTrashed()->count();
-        } catch (\Exception $e) {
-          $existing_while = static::where($slug_field, $value . '-' . $x)->where($primaryKey, '!=', $this->getKey())->count();
-        }
-        while ($existing_while) {
-          $x++;
-        }
-        $value .= '-'. $x;
-      }
-    } else {
-      //new
-      try {
-        $new = static::where($slug_field, $value)->withTrashed()->count();
-      } catch (\Exception $e) {
-        $new = static::where($slug_field, $value)->count();
-      }
-      if ($new) {
-        $x = 1;
-        try {
-          $new_while = static::where($slug_field, $value . '-' . $x)->withTrashed()->count();
-        } catch (\Exception $e) {
-          $new_while = static::where($slug_field, $value . '-' . $x)->count();
-        }
-        while ($new_while) {
-          $x++;
-        }
-        $value .= '-'. $x;
-      }
-    }
-
-    return $this->attributes[$slug_field] = $value;
-  }
 
 }
